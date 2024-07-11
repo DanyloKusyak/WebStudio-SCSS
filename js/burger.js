b4e247558d1435c86943a001dfc78b1e0f5aebce
DanyloKusyak <147238032+DanyloKusyak@users.noreply.github.com>
2024-07-11 20:13:36 +0300

    fix burger menu

--- js/burger.js
+++ js/burger.js
@@ -1,9 +1,11 @@
 (() => {
     const menuBtn = document.querySelector('.menu-button');
     const menu = document.querySelector('.navigation');
+
   
     menuBtn.addEventListener('click', function () {
       menuBtn.classList.toggle('active');
       menu.classList.toggle('active');
+
     })
   })();
